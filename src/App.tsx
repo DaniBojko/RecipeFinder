699ad289919f03c63f58eb78e4ba70d9103e3c3a
Dani Bojko <bojko.dani2@gmail.com>
2023-09-07 15:07:12 +0200

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { ChakraProvider, Show } from "@chakra-ui/react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Button, ChakraProvider, Hide, Show } from "@chakra-ui/react";
+import { Grid, GridItem, extendTheme } from "@chakra-ui/react";
 import { useState } from "react";
 import IngredientSearcher from "./components/IngredientSearcher";
+import { useWindowSize } from "./hooks/useWindowSize";
 
 function App() {
   const [ingredients, updateIngredients] = useState(["vaj", "sajt"]);
+  const [ComponentState, updateComponentState] = useState(0);
+  const [windowWidth, windowHeight] = useWindowSize();
+
+  const breakpoints = {
+    base: "0px",
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1536px",
+  };
+  extendTheme({ breakpoints });
 
   const deleteIngredient = (deleteById: number) => {
     console.log(deleteById);
@@ -19,31 +32,67 @@ function App() {
     if (item) updateIngredients([item, ...ingredients]);
   };
 
+  const returnIngredientSearcher = () => {
+    return (
+      <IngredientSearcher
+        ingredients={ingredients}
+        deleteIngredient={deleteIngredient}
+        addIngredient={addIngredient}
+      />
+    );
+  };
+
+  const returnMain = () => {
+    return (
+      <p>
+        {windowWidth} {windowHeight}
+      </p>
+    );
+  };
+
+  const IngredientOrMain = () => {
+    if (ComponentState % 2 == 0 || windowWidth >= 768) return returnMain();
+
+    return returnIngredientSearcher();
+  };
+
   return (
     <ChakraProvider>
       <Grid
         templateAreas={{ base: `"nav" "main"`, md: `"nav nav" "aside main"` }}
-        gridTemplateRows={"7vh 92vh"}
+        gridTemplateRows={"auto 1fr"}
         gridTemplateColumns={{ base: "1fr", md: "30% 1fr", lg: "300px 1fr" }}
         gap="1"
         color="blackAlpha.700"
         fontWeight="bold"
+        height="100vh"
       >
-        <GridItem pl="2" bg="orange.300" area={"nav"}>
-          Nav
+        <GridItem pl="2" padding="0.5rem" bg="orange.300" area={"nav"}>
+          <Hide above="md">
+            <Button
+              colorScheme="teal"
+              variant="solid"
+              onClick={() => updateComponentState(ComponentState + 1)}
+            >
+              Button
+            </Button>
+          </Hide>
         </GridItem>
+
         <Show above="md">
           <GridItem overflow="auto" padding="0.5rem" pl="2" area={"aside"}>
-            <IngredientSearcher
-              ingredients={ingredients}
-              deleteIngredient={deleteIngredient}
-              addIngredient={addIngredient}
-            />
+            {returnIngredientSearcher()}
           </GridItem>
         </Show>
 
-        <GridItem overflow="auto" pl="2" bg="green.300" area={"main"}>
-          Main
+        <GridItem
+          overflow="auto"
+          bg="green.300"
+          padding="0.5rem"
+          pl="2"
+          area={"main"}
+        >
+          {IngredientOrMain()}
         </GridItem>
       </Grid>
     </ChakraProvider>
@@ -51,3 +100,7 @@ function App() {
 }
 
 export default App;
+
+/*
+
+      */
